5923e742002bb05ec85207b54abda4b61db070ae
mi-kei-la <micaela.percovich@gmail.com>
2020-09-16 17:18:47 +0000

    update

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+main()
+{
+printf("Size of a char: %c byte(s)\n")
+printf("Size of an int: %d byte(s)\n");
+printf("Size of a long int: %ld byte(s)\n");
+printf("Size of a long long int: %lld byte(s)\n");
+printf("Size of a float: %f byte(s)\n");
+return (0);
+}
