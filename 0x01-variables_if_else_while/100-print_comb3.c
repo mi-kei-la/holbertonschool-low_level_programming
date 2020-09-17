0e3ddcb71ef5c735617d0d2fb813ae9edfe330a2
mi-kei-la <micaela.percovich@gmail.com>
2020-09-17 21:27:27 +0000

    update file to betty requirements

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,19 +12,19 @@ int main(void)
 int n;
 int m;
 
- for (n = 48; n <= 57; n++)
-   {
-     for (m = n+1; m <= 57; m++)
-       {
-	 putchar(n);
-	 putchar(m);
-	 if (n != 56)
-	   {
-	     putchar(44);
-	     putchar(32);
-	   }
-       }
-   }
+for (n = 48; n <= 57; n++)
+{
+for (m = n + 1; m <= 57; m++)
+{
+putchar(n);
+putchar(m);
+if (n != 56)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
 
 putchar(10);
 return (0);
