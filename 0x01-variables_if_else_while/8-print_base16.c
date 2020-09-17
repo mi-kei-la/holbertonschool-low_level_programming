51e6608bcc582847b367de9db88e49f557629ccb
mi-kei-la <micaela.percovich@gmail.com>
2020-09-17 18:54:13 +0000

    add file

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+int n;
+char a;
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+}
+for (a = 97; a <= 102; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
