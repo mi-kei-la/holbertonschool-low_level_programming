c3b7f93468658c068844a316080af60014380f17
mi-kei-la <micaela.percovich@gmail.com>
2020-09-17 12:23:32 +0000

    advanced task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * Define
- * this function
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-write
 
-return(1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+
+return (1);
 }
