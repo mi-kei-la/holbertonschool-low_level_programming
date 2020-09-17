39d6cc677888135d7fff32199893c3972adf9e68
mi-kei-la <micaela.percovich@gmail.com>
2020-09-17 15:10:03 +0000

    add files

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+main()
+{
+
+char c
+char n = '\n';
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+putchar(n);
+return (0);
+}
