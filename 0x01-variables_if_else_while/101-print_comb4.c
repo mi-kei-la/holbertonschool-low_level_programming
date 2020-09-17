e8fcfba4326fd41a241edd52e0f309deaf5b272b
mi-kei-la <micaela.percovich@gmail.com>
2020-09-17 21:58:16 +0000

    advanced

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+int n;
+int m;
+int o;
+
+for (n = 48; n <= 57; n++)
+{
+for (m = n + 1; m <= 57; m++)
+{
+for (o = m + 1; o <= 57; o++)
+{
+putchar(n);
+putchar(m);
+putchar(o);
+if (n != 55)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+putchar(10);
+return (0);
+}
