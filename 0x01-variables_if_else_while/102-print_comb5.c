de78d4d7af2762a0890427a4d495d2495534c729
mi-kei-la <micaela.percovich@gmail.com>
2020-09-18 00:07:29 +0000

    add advanced

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+int a;
+int b;
+int c;
+int d;
+
+for (a = 48; a <= 57; a++)
+{
+for (b = 48; b <= 57; b++)
+{
+for (c = 48; c <= 57; c++)
+{
+for (d = 48; d <= 57; d++)
+{
+if (a < c || (a == c && b < d))
+{
+putchar(a);
+putchar(b);
+putchar(32);
+putchar(c);
+putchar(d);
+if (a != 57 || b != 56 || c != 57 || d != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+}
+}
+putchar(10);
+return (0);
+}
