39d666891bf212a7297a047b4bd43e642d0ca430
mi-kei-la <micaela.percovich@gmail.com>
2020-09-22 18:37:02 +0000

    add files

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - entry point
+ *
+ * Print numbers to 98
+ *
+ * Return: 0 (success)
+ *
+ * @n: parameter
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+	if (n == 98)
+	{
+		printf("%d\n", n);
+	}
+}
