0e1b3d79d208581dbbd7b712a714f03f858ba8c4
mi-kei-la <micaela.percovich@gmail.com>
2020-09-22 17:37:10 +0000

    update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,37 +23,23 @@ void times_table(void)
 			y = res / 10;
 			z = res % 10;
 
-			if (y == 0)
+			if (y != 0)
 			{
-				if (b < 9)
-				{
-					_putchar(z + '0');
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
-				else
-				{
-					_putchar(z + '0');
-					_putchar('\n');
-				}
+				_putchar(y + '0');
 			}
 			else
 			{
-				if (b < 9)
-				{
-					_putchar(y + '0');
-					_putchar(z + '0');
-					_putchar(',');
-					_putchar(' ');
-				}
-				else
-				{
-					_putchar(y + '0');
-					_putchar(z + '0');
-					_putchar('\n');
-				}
-					
+				_putchar(' ');
+			}
+			_putchar(z + '0');
+			if (b != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('\n');
 			}
 		}
 
