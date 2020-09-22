43056e648421e7dc3ef71b59ae6c5f7862ca2499
mi-kei-la <micaela.percovich@gmail.com>
2020-09-22 18:47:38 +0000

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -20,16 +20,18 @@ void print_to_98(int n)
 			printf("%d, ", n);
 			n++;
 		}
+		printf("%d\n", n);
 	}
-	else
+	else if (n > 98)
 	{
-		while (n >= 98)
+		while (n > 98)
 		{
 			printf("%d, ", n);
 			n--;
 		}
+		printf("%d\n", n);
 	}
-	if (n == 98)
+	else
 	{
 		printf("%d\n", n);
 	}
