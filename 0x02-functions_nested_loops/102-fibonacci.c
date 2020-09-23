cee5a9b9f09eac2f84b682eb9aa2355f39d498a3
mi-kei-la <micaela.percovich@gmail.com>
2020-09-23 21:38:04 +0000

    add try

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * fibonacci - entry point
+ *
+ * print first 50 fibonacci numbers
+ */
+
+void fibonacci(void)
+{
+	int a;
+	unsigned int fib = 1;
+	signed long int fib2;
+	unsigned int fib_ant = 0;
+
+	for (a = 0; a <= 50; a++)
+	{
+		fib2 = fib + fib_ant;
+		printf(fib2);
+		fib_ant = fib;
+		fib = fib2;
+
+		if (a != 50)
+			printf(", ");
+	}
+}
