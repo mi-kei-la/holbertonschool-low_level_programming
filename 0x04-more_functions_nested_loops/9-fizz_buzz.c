d2f671295a78317f80405a80c34a00585332a55d
mi-kei-la <micaela.percovich@gmail.com>
2020-09-24 18:42:59 +0000

    update

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -25,7 +25,7 @@ void fizz_buzz(void)
 
 	for (count = 1; count <= 100; count++)
 	{
-		if (count % 15 == 0)
+		if ((count % 3 == 0) && (count % 5 == 0))
 			printf("FizzBuzz ");
 		else if (count % 5 == 0)
 			printf("Buzz ");
