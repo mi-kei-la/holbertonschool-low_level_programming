3c81ad804457a5af100abdbba066e42702790a09
mi-kei-la <micaela.percovich@gmail.com>
2020-09-24 17:00:37 +0000

    update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,14 +8,20 @@ void more_numbers(void)
 {
 	int nums;
 	int loop;
+	int first;
+	int last;
 
 	for (loop = 0; loop <= 10; loop++)
 	{
 		for (nums = 0; nums < 15; nums++)
 		{
-			if (nums < 10)
-				_putchar((nums / 10) + 48);
-			_putchar((nums % 10) + 48);
+			first = nums / 10;
+			last = nums % 10;
+			if (nums > 9)
+			{
+				_putchar(first + 48);
+			}
+			_putchar(last + 48);
 		}
 		_putchar('\n');
 	}
