1a87485c2b1d11de7a2e981ea6441037f416d471
mi-kei-la <micaela.percovich@gmail.com>
2020-09-25 19:28:53 +0000

    add final

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+
+/**
+ * print_number - print numbers one char each
+ *
+ * @n: parameter
+ */
+
+void print_number(int n)
+{
+	int ini;
+	unsigned long div10;
+	int num;
+	int count;
+	int exp;
+
+	if (n < 0) /* take into account negative numbers */
+	{
+		n = -n;
+		_putchar('-');
+	}
+
+	num = n;
+	ini = n;
+	count = 0;
+	while (ini > 9) /* count number of digits */
+	{
+		ini = num / 10;
+		count++;
+		num = ini;
+	}
+
+	div10 = 10;
+	exp = count - 1;
+	while (exp > 0) /* elevate 10 to the number of digits the number has */
+	{
+		div10 = div10 * 10;
+		exp--;
+	}
+
+	while (count >= 0) /* take number apart & print each digit */
+	{
+		_putchar((n / div10) + '0');
+		n = n % div10;
+		div10 = div10 / 10;
+		count--;
+	}
+}
