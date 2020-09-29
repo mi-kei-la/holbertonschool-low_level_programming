7529e2bdfed74260d6fb299da8254a1b953d09cf
mi-kei-la <micaela.percovich@gmail.com>
2020-09-29 19:48:01 +0000

    add

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy - copy a string into another
+ *
+ * @src: source parameter
+ * @dest: parameter destination
+ *
+ * Return: pointer to destination
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int count, x;
+
+	for (count = 0; src[count] != '\0'; count++)
+	{
+	}
+
+	for (x = 0; x != count; x++)
+	{
+		dest[x] = src[x];
+	}
+
+	return (dest);
+}
