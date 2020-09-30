56b0c4b28b11d3737e615b4894e7fed606461c1e
mi-kei-la <micaela.percovich@gmail.com>
2020-09-30 17:13:50 +0000

    go back

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,10 +9,7 @@
 void print_number(int n)
 {
 	int ini;
-	unsigned int div10_1 = 10;
-	unsigned int div10_2 = 10;
-	unsigned int div10_3 = 10;
-	unsigned int div10_4 = 10;
+	unsigned int div10;
 	int num;
 	int count;
 	int exp;
@@ -33,78 +30,19 @@ void print_number(int n)
 		num = ini;
 	}
 
+	div10 = 10;
 	exp = count - 1;
-	if (exp >= 18)
+	while (exp > 0) /* elevate 10 to the number of digits the number has */
 	{
-		while (exp > 18) /* elevate 10 to the number of digits the number has */
-		{
-			div10_4 = div10_4 * 10;
-			exp--;
-		}
-	}
-	else if (exp >= 12)
-	{
-		while (exp > 12)
-		{
-			div10_3 = div10_3 * 10;
-			exp--;
-		}
-	}
-	else if (exp >= 6)
-	{
-		while (exp > 6)
-		{
-			div10_2 = div10_2 * 10;
-			exp--;
-		}
-	}
-	else
-	{
-		while (exp > 0)
-		{
-			div10_1 = div10_1 * 10;
-			exp--;
-		}
+		div10 = div10 * 10;
+		exp--;
 	}
 
-	if (count >= 18)
+	while (count >= 0) /* take number apart & print each digit */
 	{
-		while (count >= 18) /* take number apart & print each digit */
-		{
-			_putchar((n / div10_4) + '0');
-			n = n % div10_4;
-			div10_4 = div10_4 / 10;
-			count--;
-		}
-	}
-	else if (count >= 12)
-	{
-		while (count >= 12)
-		{
-			_putchar((n / div10_3) + '0');
-			n = n % div10_3;
-			div10_3 = div10_3 / 10;
-			count--;
-		}
-	}
-	else if (count >= 6)
-	{
-		while (count >= 6)
-		{
-			_putchar((n / div10_2) + '0');
-			n = n % div10_2;
-			div10_2 = div10_2 / 10;
-			count--;
-		}
-	}
-	else
-	{
-		while (count >= 0)
-		{
-			_putchar((n / div10_1) + '0');
-			n = n % div10_1;
-			div10_1 = div10_1 / 10;
-			count--;
-		}
+		_putchar((n / div10) + '0');
+		n = n % div10;
+		div10 = div10 / 10;
+		count--;
 	}
 }
