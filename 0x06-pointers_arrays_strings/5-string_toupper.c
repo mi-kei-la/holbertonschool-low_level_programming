f49efee9e8c0a41b2156de026b7d805f49ce0a09
mi-kei-la <micaela.percovich@gmail.com>
2020-10-01 17:23:07 +0000

    add

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - change string to upper
+ *
+ */
+
+char *string_toupper(char *n)
+{
+	int x;
+
+	for (x = 0; n[x] != '\0'; x++)
+	{
+		if (n[x] >= 97 && n[x] <= 122)
+			n[x] = n[x] - 32;
+	}
+
+	return (n);
+}
