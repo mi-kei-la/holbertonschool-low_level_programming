750570e037675cc61f9d5029e00dcc052b0a1cf6
mi-kei-la <micaela.percovich@gmail.com>
2020-10-01 16:29:59 +0000

    update

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -28,7 +28,7 @@ char *_strncat(char *dest, char *src, int n)
 	for (i = 0 ; i < n && src[i] != '\0' ; i++)
 	{
 		dest[dest_len + i] = src[i];
-		if (src_len < n)
+		if (src_len <= n)
 			dest[dest_len + i] = '\0';
 	}
 	return (dest);
