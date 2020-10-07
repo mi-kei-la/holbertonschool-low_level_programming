229de61994273914ed3168b80c94ef36de2c2100
mi-kei-la <micaela.percovich@gmail.com>
2020-10-07 19:30:11 +0000

    add

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * _sqrt_recursion - find the natural square root of a number
+ *
+ * @n: parameter
+ *
+ * Return: square root, or -1 if not found
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (0);
+	else if (n == 1)
+		return (1);
+	return (_rec(n, 2));
+}
+
+int _rec(int n, int b)
+{
+	if (b * b == n)
+		return (b);
+	else if (b * b > n)
+		return (-1);
+	return (_rec(n, b + 1));
+}
