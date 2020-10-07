cc5a16a68fefc96567c629dfe93d8b12ab247116
mi-kei-la <micaela.percovich@gmail.com>
2020-10-07 20:01:17 +0000

    add

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * is_prime_number - check if number is prime
+ *
+ * @n: number
+ *
+ * Return: 1 if true, 0 otherwise
+ */
+
+int is_prime_number(int n)
+{
+	int x = 0;
+
+	if (n <= 1)
+		return (0);
+
+	x = _check(n, 2);
+
+	if (x != 0)
+		return (1);
+	else
+		return (0);
+}
+
+int _check(int n, int i)
+{
+	if (n == i)
+		return (1);
+	else if (n % i == 0)
+		return (0);
+	else
+		return (_check(n, i + 1));
+}
