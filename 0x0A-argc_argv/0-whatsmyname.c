9b463994d7d3c85ee77540bb79c97f5e9c50f709
mi-kei-la <micaela.percovich@gmail.com>
2020-10-09 20:14:58 +0000

    add

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - print file name
+ *
+ * @argc: parameter
+ * @argv: parameter
+ *
+ * Return: 0 (success)
+ */
+
+int main(__attribute__((unused))int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * main - entry point
+ *
+ * @argc: parameter
+ * @argv: parameter
+ *
+ * Return: 0 (success)
+ */
+
+int main(int argc, __attribute__((unused))char **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
