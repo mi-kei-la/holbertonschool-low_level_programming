16265ce1201ec821c304baf3f8e8cfab114a4fc6
mi-kei-la <micaela.percovich@gmail.com>
2020-10-15 16:28:22 +0000

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,11 +22,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (len1 = 0; s1[len1] != 0; len1++) /* count length of s1 */
+	for (len1 = 0; s1[len1] != 0; len1++) /* count strings */
 	{
 	}
 
-	for (len2 = 0; s2[len2] != 0 && len2 <= n; len2++) /* count length of s2 up to n */
+	for (len2 = 0; s2[len2] != 0 && len2 < n; len2++)
 	{
 	}
 
