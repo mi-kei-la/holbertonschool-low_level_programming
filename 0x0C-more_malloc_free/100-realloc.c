5f6f8b1fe2f09bce78f20696357b2365c2964a7a
mi-kei-la <micaela.percovich@gmail.com>
2020-10-15 20:06:12 +0000

    update

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,9 +16,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (old_size == new_size)
 		return (ptr);
 
-	if (ptr == NULL)
-		ptr = malloc(new_size);
-
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
