e974e49a156cff0fc7c8b4dabfc36338c4a41fb2
mi-kei-la <micaela.percovich@gmail.com>
2020-10-15 19:56:23 +0000

    add advanced task

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _realloc - reallocate a memory block
+ *
+ * @ptr: pointer to old_size
+ * @old_size: parameter
+ * @new_size: parameter
+ * Return: pointer to new_size, NULL otherwise
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if (old_size == new_size)
+		return (ptr);
+
+	if (ptr == NULL)
+		ptr = malloc(new_size);
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size < old_size)
+		free(ptr);
+
+	ptr = malloc(new_size);
+
+	return (ptr);
+}
