64d88df84372db3ef3694e4b9bc1d583498cfd1f
mi-kei-la <micaela.percovich@gmail.com>
2020-10-15 16:07:04 +0000

    add

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenate two strings until n
+ *
+ * @s1: first string
+ * @s2: second string
+ * @n: parameter
+ *
+ * Return: pointer to new string, NULL otherwise
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+}
