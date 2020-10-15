80e3d9a5b8c81b270d4f4065047b6665ef689bf2
mi-kei-la <micaela.percovich@gmail.com>
2020-10-15 16:26:29 +0000

    add task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,5 +13,34 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int len1, len2, x, y;
+	char *new;
 
+	if (s1 == NULL) /* if NULL is passed, treat it as an empty string */
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (len1 = 0; s1[len1] != 0; len1++) /* count length of s1 */
+	{
+	}
+
+	for (len2 = 0; s2[len2] != 0 && len2 <= n; len2++) /* count length of s2 up to n */
+	{
+	}
+
+	new = malloc(sizeof(char) * (len1 + len2) + 1); /* assign mem, flag */
+	if (new == NULL)
+		return (NULL);
+
+	for (x = 0; s1[x] != 0; x++)
+		new[x] = s1[x];
+
+	for (y = 0; s2[y] != 0 && y < n; y++)
+		new[x + y] = s2[y];
+
+	new[x + y] = '\0';
+
+	return (new);
 }
