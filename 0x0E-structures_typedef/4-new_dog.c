724670e7b3701afd38058e23192417c40adbee48
mi-kei-la <micaela.percovich@gmail.com>
2020-10-20 02:37:00 +0000

    add

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,78 +3,81 @@
 #include <stdlib.h>
 
 /**
- * new_dog - create a new dog
+ * _strdup - array of chars
+ * @str: string
  *
- * @name: dog name
- * @age: dog age
- * @owner: dog owner
- * Return: pointer to new dog
- */
-
-dog_t *new_dog(char *name, float age, char *owner)
-{
-	char *new_name, *new_owner;
-	dog_t *dog;
-
-	if (name == NULL || owner == NULL)
-		return (0);
-
-	dog = malloc(sizeof(dog_t));
-
-	if (dog == NULL)
-		return (NULL);
-
-	new_name = _strdup(name);
-	if (new_name == NULL)
-	{
-		free(dog);
-		return (0);
-	}
-
-	new_owner = _strdup(owner);
-	if (new_owner == NULL)
-	{
-		free(dog);
-		free(new_name);
-		return (NULL);
-	}
-
-	dog->name = new_name;
-	dog->age = age;
-	dog->owner = new_owner;
-
-	return (dog);
-}
-
-
-/**
-* _strdup - copy string, return pointer to it
- *
- * @str: original string
- *
- * Return: pointer to duplicated string, NULL otherwise
+ * Return: always
  */
 
 char *_strdup(char *str)
 {
-	int i, j;
-	char *new;
+	char *s; /*pointer to copy of str*/
+	int i; /*index of str*/
+	int ii; /*index of copy of str*/
 
 	if (str == NULL)
+	{
 		return (NULL);
+	}
 
-	for (i = 0; str[i] != 0; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 	}
 
-	new = malloc((sizeof(char) * i) + 1);
+	s = malloc(i * sizeof(char) + 1);
 
-	if (new == NULL)
+	if (s == NULL)
+	{
 		return (NULL);
+	}
+	for (ii = 0; ii <= i; ii++)
+	{
+		s[ii] = str[ii];
+	}
+	return (s);
+}
 
-	for (j = 0; j != i; j++)
-		new[j] = str[j];
+/**
+ * new_dog - dog description
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Return: dogs description
+ */
 
-	return (new);
-	free(new);
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	char *cpy_name, *cpy_owner;
+
+	if (name == NULL || owner == NULL)
+	{
+		return (0);
+	}
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+	{
+		return (NULL);
+	}
+
+	cpy_name = _strdup(name);
+	if (cpy_name == NULL)
+	{
+		free(dog);
+		return (0);
+	}
+	cpy_owner = _strdup(owner);
+	if (cpy_owner == NULL)
+	{
+		free(dog);
+		free(cpy_name);
+		return (NULL);
+	}
+
+	dog->name = cpy_name;
+	dog->age = age;
+	dog->owner = cpy_owner;
+	return (dog);
 }
