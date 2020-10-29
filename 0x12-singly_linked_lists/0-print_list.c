0cbbfc2aebde41375802fdbd331ce97b559ccb06
mi-kei-la <micaela.percovich@gmail.com>
2020-10-29 16:43:21 +0000

    add

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,6 +3,14 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+  * print_list - print all elements of linked list
+  *
+  * @h: pointer
+  *
+  * Return: count of nodes
+  */
+
 size_t print_list(const list_t *h)
 {
 	list_t *aux = malloc(sizeof(list_t));
