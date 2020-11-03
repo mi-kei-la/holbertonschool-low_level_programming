516b69301034c723a1ba8191bdb00a2e101b27c4
mi-kei-la <micaela.percovich@gmail.com>
2020-11-03 18:38:48 +0000

    new try

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -18,9 +18,9 @@ void free_list(list_t *head)
 
 	while (head)
 	{
-		tmp = head->next;
-		free(head->str);
-		free(head);
-		head = tmp;
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
 	}
 }
