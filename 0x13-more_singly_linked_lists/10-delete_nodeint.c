29867e994127bf56f9aa504fc64a9b49843e6fd9
mi-kei-la <micaela.percovich@gmail.com>
2020-11-03 21:53:48 +0000

    update

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -19,7 +19,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = temp->next;
-		free(head);
+		free(temp);
 		return (1);
 	}
 
@@ -30,9 +30,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	if (temp == NULL || temp->next == NULL)
 		return (-1);
-	aux = temp->next;
-	aux->next = temp->next->next;
-	free(temp);
-	temp->next = aux->next;
+	aux = temp->next->next;
+	free(temp->next);
+	temp->next = aux;
 	return (1);
 }
