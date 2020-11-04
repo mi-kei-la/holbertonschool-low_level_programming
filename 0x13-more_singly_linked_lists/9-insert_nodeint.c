8a29fe9769bea910edb7c804cf4167cc3bdeae64
mi-kei-la <micaela.percovich@gmail.com>
2020-11-04 18:16:17 +0000

    add

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 
-	tmp = *head;
 	new->n = n;
 
 	if (*head == NULL)
@@ -29,6 +28,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new->next = NULL;
 		return (new);
 	}
+
+	tmp = *head;
 
 	if (idx == 0)
 	{
@@ -44,9 +45,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			tmp = tmp->next;
 			count++;
 		}
+		new->next = tmp->next;
+		tmp->next = new;
 	}
-
-	new->next = tmp->next;
-	tmp->next = new;
 	return (new);
 }
