1756f01c70df8134e4c1a5f1bdc46f3f0234543d
mi-kei-la <micaela.percovich@gmail.com>
2020-11-04 18:22:10 +0000

    update

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,13 +10,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *aux;
 
-	if (*head == NULL)
+	if (!*head)
+	{
+		*head = NULL;
 		return;
+	}
 	while (*head)
 	{
 		aux = (**head).next;
 		free(*head);
 		*head = aux;
 	}
-	*head = NULL;
 }
