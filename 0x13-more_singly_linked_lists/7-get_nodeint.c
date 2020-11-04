81110ecda44d9dff110b482b712338e1e786b58c
mi-kei-la <micaela.percovich@gmail.com>
2020-11-04 17:13:42 +0000

    update

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,8 +12,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int count = 0;
 
-	if (head == NULL)
-		return (NULL);
 	while (count < index)
 	{
 		head = head->next;
