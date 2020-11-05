a2d492c8de0c4d2be072eacdec29a7b7534cbe67
mi-kei-la <micaela.percovich@gmail.com>
2020-11-05 17:06:27 +0000

    add files

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+
+/**
+  * binary_to_uint - convert to decimal
+  *
+  * @b: binary number in form of a string
+  *
+  * Return: unsigned decimal
+  */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int div = 1, sum = 0, i, j, count = 0;
+
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; b[i] != 0 && b[i] == 0; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+	for (; b[i] != 0; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		count++;
+	}
+
+	while (count > 1)
+	{
+		div = div * 2;
+		count--;
+	}
+
+	for (j = 0; b[j] != 0; j++)
+	{
+		if (b[j] == '1')
+			sum += div;
+		div = div / 2;
+	}
+	return (sum);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,17 @@
+# Bit manipulation
+
+This directory contains all files pertaining the first project about bit manipulation in the C language.
+
+## Tasks
+
+- 0 - Convert binary to integer
+
+- 1 - Print binary representation of a number
+
+- 2 - Return value of a bit at a given index
+
+- 3 - Set a bit to 1 at a given index
+
+- 4 - Set a bit to 0 at a given index
+
+- 5 - Return number of bits you would need to flip to get from one number to another
