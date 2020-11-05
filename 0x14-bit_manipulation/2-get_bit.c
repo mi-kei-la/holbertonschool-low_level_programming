17052f4622b2a0ccf11dafeee3a897291d94e51f
mi-kei-la <micaela.percovich@gmail.com>
2020-11-05 21:32:28 +0000

    add

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+  * get_bit - get value of bit at given index
+  *
+  * @n: parameter
+  * @index: index
+  *
+  * Return: value of bit, -1 otherwise
+  */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask = 1;
+
+	if (index > n)
+		return (-1);
+	if (n == 0)
+		return (0);
+	mask = mask << index;
+	if ((n & mask) == 0)
+		return (0);
+	else if ((n & mask) != 0)
+		return (1);
+	else
+		return (-1);
+}
