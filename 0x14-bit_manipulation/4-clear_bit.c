1d752b9f3a903025ee8f256fa0d1ce6c98a235d1
mi-kei-la <micaela.percovich@gmail.com>
2020-11-05 23:51:59 +0000

    add 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+  * clear_bit - set specific bit to 0
+  *
+  * @n: pointer to int
+  * @index: position of bit
+  *
+  * Return: 1 if it worked, -1 otherwise
+  */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask = 1;
+
+	mask = mask << index;
+	mask = ~ mask;
+	*n  = *n & mask;
+	return (1);
+
+}
