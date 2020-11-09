a59731cca011f1186da976a7e875e63a450eaab1
mi-kei-la <micaela.percovich@gmail.com>
2020-11-09 18:59:44 +0000

    add

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,12 +12,11 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, strlen;
-	unsigned int flag = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_TRUNC| O_RDWR, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	if (fd == -1)
 		return (-1);
 
@@ -27,12 +26,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		flag = write(fd, text_content, strlen);
+		for (strlen = 0; text_content[strlen] != 0; strlen++)
+		{
+		}
+		write(fd, text_content, (strlen + 1));
 	}
 	close(fd);
 
-	if (flag != 0)
-		return (1);
-	return (-1);
+	return (1);
 
 }
