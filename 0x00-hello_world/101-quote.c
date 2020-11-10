0144e67f17ef11a70fa76f47e6b147475f7b25a6
mi-kei-la <micaela.percovich@gmail.com>
2020-11-10 17:56:34 +0000

    add

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,6 @@
 
 int main(void)
 {
-
-	write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-
+	write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
