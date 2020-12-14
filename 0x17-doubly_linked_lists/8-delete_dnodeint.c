986deee090dcf07f4bba9972cf121fe2949bb74f
mi-kei-la <micaela.percovich@gmail.com>
2020-12-14 16:57:21 +0000

    add files

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -28,6 +28,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0) /* delete first node */
 	{
 		tmp->next->prev = NULL;
+		*head = (*head)->next;
 		free(tmp);
 		return (1);
 	}
