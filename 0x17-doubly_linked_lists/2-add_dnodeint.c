bd3fe33b6aaf44bf6c99258f1f79eef0edf458ac
mi-kei-la <micaela.percovich@gmail.com>
2020-12-14 14:09:41 +0000

    update

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -20,12 +20,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new == NULL) /* check for if allocation fails */
 		return (NULL);
 
-	if ((*head)->prev != NULL)
-	{
-		while ((*head)->prev != NULL)
-			*head = (*head)->prev;
-	}
-
 	new->prev = NULL;
 	new->n = n;
 	new->next = *head;
