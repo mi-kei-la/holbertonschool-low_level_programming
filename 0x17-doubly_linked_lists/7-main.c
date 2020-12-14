5d201167de8fec1547422578fbd7dbdc2f644fce
mi-kei-la <micaela.percovich@gmail.com>
2020-12-14 05:14:00 +0000

    update

--- 0x17-doubly_linked_lists/7-main.c
+++ 0x17-doubly_linked_lists/7-main.c
@@ -22,6 +22,15 @@ int main(void)
     printf("-----------------\n");
     insert_dnodeint_at_index(&head, 5, 4096);
     print_dlistint(head);
+    printf("-----------------\n");
+    insert_dnodeint_at_index(&head, 8, 1020);
+    print_dlistint(head);
+     printf("-----------------\n");
+    insert_dnodeint_at_index(&head, 9, 4096);
+    print_dlistint(head);
+     printf("-----------------\n");
+    insert_dnodeint_at_index(&head, 0, 4096);
+    print_dlistint(head);
     free_dlistint(head);
     head = NULL;
     return (EXIT_SUCCESS);
