a512464f3503f9285229ac488ac2eec0672926f2
mi-kei-la <micaela.percovich@gmail.com>
2020-12-14 20:31:31 +0000

    upload all files

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,71 @@
+#include "functions.h"
+
+/**
+* add - add two integers
+*
+* @a: parameter
+* @b: parameter
+*
+* Return: result
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* sub - substract two integers
+*
+* @a: parameter
+* @b: parameter
+*
+* Return: result
+*/
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* mul - multiply two integers
+*
+* @a: parameter
+* @b: parameter
+*
+* Return: result
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* div - divide two integers
+*
+* @a: parameter
+* @b: parameter
+*
+* Return: result
+*/
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* mod - module of two integers
+*
+* @a: parameter
+* @b: parameter
+*
+* Return: result
+*/
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
