84979d7c28fd57ed56d7caa27b2b39f77f3290e3
mi-kei-la <micaela.percovich@gmail.com>
2021-04-20 18:34:04 +0000

    refactor code to remove recursive calls

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,40 +1,50 @@
 #include "search_algos.h"
 
 /**
- * interpolation_search - searches value in sorted array
- * using Interpolation search
- * @array: pointer to first element of array
- * @size: number of elements in array
- * @value: value to search for
+ * interpolation_search - Search a value in an array by interpolation.
  *
- * Return: index where value is located
- * -1 if value not present or array is NULL
- * Prints array being searched every time it changes
+ * @array: Array to search in.
+ * @size: Size of the array.
+ * @value: Value being searched.
+ *
+ * Return: Index of value, -1 if not found or no array.
  */
+
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t pos;
-	int high = size - 1, low = 0;
+	size_t low = 0, high = size - 1, pos;
 
 	if (!array || size == 0)
 		return (-1);
 
+	pos = low + (((double)(high - low) / (array[high] - array[low]))
+			* (value - array[low]));
+
 	while (low <= high && value >= array[low] && value <= array[high])
 	{
+		printf("Value checked array[%zu] = [%d]\n", pos, array[pos]);
+
 		if (low == high)
-			return (array[low] == value ? low : -1);
-		pos = low + (((double)(high - low) / (array[high] - array[low]))
-			     * (value - array[low]));
-		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+		{
+			if (array[low] == value)
+				return (low);
+			return (-1);
+		}
+
 		if (array[pos] == value)
 			return (pos);
+
 		if (array[pos] < value)
 			low = pos + 1;
-		else
+
+		if (array[pos] > value)
 			high = pos - 1;
+
+		pos = low + (((double)(high - low) / (array[high] - array[low]))
+			* (value - array[low]));
 	}
-	pos = low + (((double)(high - low) / (array[high] - array[low]))
-		     * (value - array[low]));
+
 	printf("Value checked array[%lu] is out of range\n", pos);
 	return (-1);
+
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,6 +8,5 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
-int recursive_interpolation(int *array, size_t low, size_t high, int value);
 
 #endif
